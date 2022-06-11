47e36abce7801c40df73b15c7340fbfa45c7f9fd
Amal <apptalid@gmail.com>
2022-06-11 12:59:14 +0300

    .

--- main.py
+++ main.py
@@ -6,23 +6,29 @@ app = Flask(__name__)
 
 # The "@" decorator associates this route with the function immediately following
 @app.route('/')
-def hello_world(num1=8, num2=8):
+def hello_world(num1=8, num2=8, color1="brown", color2="red"):
     # Return 8 by 8 checkerboard
-    return render_template('index.html', num1=int(num1), num2=int(num2))
+    return render_template('index.html', num1=int(num1), num2=int(num2), color1=color1, color2=color2)
 
 
 # The "@" decorator associates this route with the function immediately following
 @app.route('/4')
-def four_times(num1=8, num2=4):
+def four_times(num1=8, num2=4, color1="brown", color2="red"):
     # Return 8 by 4 checkerboard
-    return render_template('index.html', num1=int(num1), num2=int(num2))
+    return render_template('index.html', num1=int(num1), num2=int(num2), color1=color1, color2=color2)
 
 
 # The "@" decorator associates this route with the function immediately following
 @app.route('/<num1>/<num2>')
-def costmized(num1, num2):
+def costmized(num1, num2, color1="brown", color2="red"):
     # Return x by y checkerboard.
-    return render_template('index.html', num1=int(num1), num2=int(num2))
+    return render_template('index.html', num1=int(num1), num2=int(num2), color1=color1, color2=color2)
+
+
+@app.route('/<num1>/<num2>/<string:color1>/<string:color2>')
+def color_costmized(num1, num2, color1, color2):
+    # Return x by y checkerboard.
+    return render_template('index.html', num1=int(num1), num2=int(num2), color1=color1, color2=color2)
 
 
 if __name__ == "__main__":   # Ensure this file is being run directly and not from a different module
